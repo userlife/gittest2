31c00b4bcee05559bb7a76b1e9febcafaa19f16d
tiz <tiz@inforex.co.kr>
2020-01-28 09:09:13 +0900

    테스트1

---
+++ app/src/main/java/com/gittester/m/app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gittester.m.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
